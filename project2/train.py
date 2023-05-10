2c9bf83d9442b54334cb4db025fdc129c0c1597d
Mei <83911312+mskmei@users.noreply.github.com>
2023-05-10 17:36:06 +0800

    Update train.py

--- project2/train.py
+++ project2/train.py
@@ -10,90 +10,99 @@ import numpy as np
 # set device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 batch_size = 128
-train_loader,valid_loader,test_loader = get_dataloader(batch_size=batch_size)
+train_loader, valid_loader, test_loader = get_dataloader(batch_size=batch_size)
 n_class = 10
 model = ResNet18()
 
-model.fc = torch.nn.Linear(512, n_class) 
+model.fc = torch.nn.Linear(512, n_class)
 model = model.to(device)
 criterion = nn.CrossEntropyLoss().to(device)
 
-
-n_epochs = 250
-valid_loss_min = np.Inf 
+EPOCH = 250
+lower = np.Inf
 accuracy = []
+train_loss_hist = []
+val_loss_hist = []
 lr = 0.1
-counter = 0
-for epoch in tqdm(range(1, n_epochs+1)):
+count = 0
+for epoch in tqdm(range(1, EPOCH + 1)):
 
     train_loss = 0.0
-    valid_loss = 0.0
+    val_loss = 0.0
     total_sample = 0
     right_sample = 0
-    
-    if counter/10 ==1:
-        counter = 0
-        lr = lr*0.5
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
- 
-    model.train() 
-    for data, target in train_loader:
-        data = data.to(device)
-        target = target.to(device)
-        optimizer.zero_grad()
-        # forward pass: compute predicted outputs by passing inputs to the model
-        # (正向传递：通过向模型传递输入来计算预测输出)
-        output = model(data).to(device)  #（等价于output = model.forward(data).to(device) ）
-        # calculate the batch loss（计算损失值）
-        loss = criterion(output, target)
-        # backward pass: compute gradient of the loss with respect to model parameters
-        # （反向传递：计算损失相对于模型参数的梯度）
-        loss.backward()
-        # perform a single optimization step (parameter update)
-        # 执行单个优化步骤（参数更新）
-        optimizer.step()
-        # update training loss（更新损失）
-        train_loss += loss.item()*data.size(0)
-        
-    ######################    
-    # 验证集的模型#
-    ######################
 
-    model.eval()  # 验证模型
-    for data, target in valid_loader:
-        data = data.to(device)
-        target = target.to(device)
-        # forward pass: compute predicted outputs by passing inputs to the model
-        output = model(data).to(device)
-        # calculate the batch loss
-        loss = criterion(output, target)
-        # update average validation loss 
-        valid_loss += loss.item()*data.size(0)
+    if count / 10 == 1:
+        count = 0
+        lr = lr * 0.5
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+
+    model.train()
+    for x, y in train_loader:
+        x = x.to(device)
+        y = y.to(device)
+        optimizer.zero_grad()
+        output = model(x).to(device)
+        loss = criterion(output, y)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item() * x.size(0)
+
+
+    model.eval()
+    for x, y in valid_loader:
+        x = x.to(device)
+        y = y.to(device)
+        output = model(x).to(device)
+        loss = criterion(output, y)
+        val_loss += loss.item() * x.size(0)
         # convert output probabilities to predicted class(将输出概率转换为预测类)
-        _, pred = torch.max(output, 1)    
+        _, pred = torch.max(output, 1)
         # compare predictions to true label(将预测与真实标签进行比较)
-        correct_tensor = pred.eq(target.data.view_as(pred))
-        # correct = np.squeeze(correct_tensor.to(device).numpy())
+        prediction = pred.eq(y.x.view_as(pred))
+        # correct = np.squeeze(prediction.to(device).numpy())
         total_sample += batch_size
-        for i in correct_tensor:
+        for i in prediction:
             if i:
                 right_sample += 1
-    print("Accuracy:",100*right_sample/total_sample,"%")
-    accuracy.append(right_sample/total_sample)
- 
-    # 计算平均损失
-    train_loss = train_loss/len(train_loader.sampler)
-    valid_loss = valid_loss/len(valid_loader.sampler)
-        
-    # 显示训练集与验证集的损失函数 
+    print("Accuracy:", 100 * right_sample / total_sample, "%")
+    accuracy.append(right_sample / total_sample)
+
+    train_loss_hist.append(train_loss)
+    val_loss_hist.append(val_loss)
+    train_loss = train_loss / len(train_loader.sampler)
+    val_loss = val_loss / len(valid_loader.sampler)
+
+    # 显示训练集与验证集的损失函数
     print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
-        epoch, train_loss, valid_loss))
-    
+        epoch, train_loss, val_loss))
+
     # 如果验证集损失函数减少，就保存模型。
-    if valid_loss <= valid_loss_min:
-        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min,valid_loss))
-        torch.save(model.state_dict(), 'checkpoint/resnet18_cifar10.pt')
-        valid_loss_min = valid_loss
-        counter = 0
+    if val_loss <= lower:
+        print('Model updated...')
+        torch.save(model.state_dict(), 'resnet18_best.pt')
+        lower = val_loss
+        count = 0
     else:
-        counter += 1
+        count += 1
+
+print(accuracy)
+print(train_loss_hist)
+print(val_loss_hist)
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.plot(np.arange(EPOCH), train_loss_hist, label="train")
+ax.plot(np.arange(EPOCH), val_loss_hist, label="val")
+ax.set_xlabel("epoch")
+ax.set_ylabel("loss")
+ax.legend()
+plt.savefig("loss.png")
+
+fig, ax = plt.subplots()
+ax.plot(np.arange(EPOCH), accuracy, label="acc")
+# ax.plot(np.arange(EPOCH), val_loss_hist, label="val")
+ax.set_xlabel("epoch")
+ax.set_ylabel("accuracy")
+ax.legend()
+plt.savefig("acc.png")
