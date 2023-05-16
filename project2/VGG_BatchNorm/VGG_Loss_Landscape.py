1bcd83f8bed76f7a87f9327a070d4dc2b51bceba
Mei <83911312+mskmei@users.noreply.github.com>
2023-05-17 02:06:42 +0800

    Update VGG_Loss_Landscape.py

--- project2/VGG_BatchNorm/VGG_Loss_Landscape.py
+++ project2/VGG_BatchNorm/VGG_Loss_Landscape.py
@@ -29,15 +29,68 @@ device_id = device_id
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 device = torch.device("cuda:{}".format(3) if torch.cuda.is_available() else "cpu")
 print(device)
-print(torch.cuda.get_device_name(3))
+# print(torch.cuda.get_device_name(3))
+device="cuda"
 
+"""
+Data loaders
+"""
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+import torchvision.datasets as datasets
+import os
+from pathlib import Path
+
+def Seek_dir(Dirname:str='JXHdatasets',plan=2,fd=Path(__file__).resolve().parent):
+    '''plan--往上查找层数      fd--运行文件父路径'''
+    origin_fd=fd
+    while plan: 
+        for p in fd.glob(Dirname): return p.as_posix()
+        plan-=1 ;fd=fd.parent
+    # 没找到文件夹，在爷路径下创建文件夹
+    print('DirPath Not Found,creating now! Path=',origin_fd.parent.as_posix()+'/'+Dirname)
+    Path(origin_fd.parent.as_posix()+'/'+Dirname).mkdir()
+    return origin_fd.parent.as_posix()+'/'+Dirname  
+
+class PartialDataset(Dataset):
+    def __init__(self, dataset, n_items=10):
+        self.dataset = dataset
+        self.n_items = n_items
+
+    def __getitem__(self):
+        return self.dataset.__getitem__()
+
+    def __len__(self):
+        return min(self.n_items, len(self.dataset))
+
+
+def get_cifar_loader(root=Seek_dir('JXHdatasets',4), batch_size=128, train=True, shuffle=True, num_workers=4, n_items=-1):
+    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                     std=[0.5, 0.5, 0.5])
+
+    data_transforms = transforms.Compose(
+        [transforms.ToTensor(),
+        normalize])
+
+    dataset = datasets.CIFAR10(root=root, train=train, download=True, transform=data_transforms)
+    if n_items > 0:
+        dataset = PartialDataset(dataset, n_items)
+
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+    return loader
 
 
 # Initialize your data loader and
 # make sure that dataloader works
 # as expected by observing one
 # sample from it.
-train_loader, val_loader, test_loader = get_dataloader(batch_size=128)
+train_loader = get_cifar_loader(train=True)
+val_loader = get_cifar_loader(train=False)
 
 
 
